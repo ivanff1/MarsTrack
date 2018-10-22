eb2f1ab843296c62036140677ddf4d123a8dde79
Ivan Zlatanov <ivanff1@abv.bg>
2018-10-23 00:03:28 +0300

    Fixed data processing

--- MarsTrack/Data Gathering/Data Analytics/Data Analytics/Data Analytics/Program.cs
+++ MarsTrack/Data Gathering/Data Analytics/Data Analytics/Data Analytics/Program.cs
@@ -11,9 +11,9 @@ namespace Data_Analytics
 {
     class Program
     {
-        static List<Tuple<string, string, string[]>> fileNamesList = new List<Tuple<string, string, string[]>>();
-        static List<Tuple<string, string, string[]>> clearList = new List<Tuple<string, string, string[]>>();
-        static string filePath = @"E:\Git Repos\MarsTrack\MarsTrack\Gathered Data";
+        static List<Tuple<string, string, string[]>> fullDataList = new List<Tuple<string, string, string[]>>();
+        static List<Tuple<string, string, List<float>>> pureDataList = new List<Tuple<string, string, List<float>>>();
+        static string filePath = @"D:\Projects\Challenges\MarsTrack\MarsTrack\Data Gathering\Gathered Data";
         
         static void Main(string[] args)
         {
@@ -24,114 +24,113 @@ namespace Data_Analytics
         static void StartDataColllection()
         {
             DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
-            foreach (var fileName in directoryInfo.GetFiles())
+            foreach (var file in directoryInfo.GetFiles())
             {
-                if (fileName.ToString().Contains("Temp"))
+                if (file.ToString().Contains("Temp"))
                 {
                     Console.Write("TEMPERATURE: ");
-                    string fileDate = Regex.Match(fileName.ToString(), @"(\([^;]*)\)").ToString();
-                    fileNamesList.Add(new Tuple<string, string, string[]>("TEMP", fileDate, File.ReadAllLines(filePath + @"\" + fileName.ToString())));
+                    string fileDate = Regex.Match(file.ToString(), @"(\([^;]*)\)").ToString();
+                    fullDataList.Add(new Tuple<string, string, string[]>("TEMP", fileDate, File.ReadAllLines(filePath + @"\" + file.ToString())));
                     Console.WriteLine("READY");
                 }
-                else if (fileName.ToString().Contains("SoilMoist"))
+                else if (file.ToString().Contains("SoilMoist"))
                 {
                     Console.Write("SOIL MOISTURE: ");
-                    string fileDate = Regex.Match(fileName.ToString(), @"(\([^;]*)\)").ToString();
-                    fileNamesList.Add(new Tuple<string, string, string[]>("SOILMOIST", fileDate, File.ReadAllLines(filePath + @"\" + fileName.ToString())));
+                    string fileDate = Regex.Match(file.ToString(), @"(\([^;]*)\)").ToString();
+                    fullDataList.Add(new Tuple<string, string, string[]>("SOILMOIST", fileDate, File.ReadAllLines(filePath + @"\" + file.ToString())));
                     Console.WriteLine("READY");
                 }
-                else if (fileName.ToString().Contains("Humid"))
+                else if (file.ToString().Contains("Humid"))
                 {
                     Console.Write("HUMIDITY: ");
-                    string fileDate = Regex.Match(fileName.ToString(), @"(\([^;]*)\)").ToString();
-                    fileNamesList.Add(new Tuple<string, string, string[]>("HUM", fileDate, File.ReadAllLines(filePath + @"\" + fileName.ToString())));
+                    string fileDate = Regex.Match(file.ToString(), @"(\([^;]*)\)").ToString();
+                    fullDataList.Add(new Tuple<string, string, string[]>("HUM", fileDate, File.ReadAllLines(filePath + @"\" + file.ToString())));
                     Console.WriteLine("READY");
                 }
                 else
                 {
                     Console.WriteLine("Invalid Data is in the file");
                 }
-                for (int i = 0; i < fileNamesList.Count; i++)
+                for (int i = 0; i < fullDataList.Count; i++)
                 {
-                    for (int j = 0; j < fileNamesList[i].Item3.Count(); j++)
-                    {
-                        if (String.IsNullOrEmpty(fileNamesList[i].Item3[j]))
-                        {
-                            Console.WriteLine("Empty");
-                        }
-                        else if (!String.IsNullOrEmpty(fileNamesList[i].Item3[j]))
-                        {
-                            if (fileNamesList[i].Item1.Equals("TEMP"))
-                            {
-                                for (int f = 0; f < fileNamesList[i].Item3.Length; f++)
-                                {
-                                    fileNamesList[i].Item3[f] = Regex.Replace(fileNamesList[i].Item3[j], "<([^;]*)>", "");
-                                }
+                    string dataType = fullDataList[i].Item1;
+                    string collectionDate = fullDataList[i].Item2;
+                    List<float> sensorData = new List<float>();
 
-                                clearList.Add(new Tuple<string, string, string[]>(fileNamesList[i].Item1, fileNamesList[i].Item2, fileNamesList[i].Item3));
-                            }
-                            else if (fileNamesList[i].Item1.Equals("HUM"))
-                            {
-                                for (int f = 0; f < fileNamesList[i].Item3.Length; f++)
-                                {
-                                    fileNamesList[i].Item3[f] = Regex.Replace(fileNamesList[i].Item3[j], "<([^;]*)>", "");
-                                }
-                                fileNamesList[i].Item3[j] = Regex.Replace(fileNamesList[i].Item3[j], "<([^;]*)>", "");
-                                clearList.Add(new Tuple<string, string, string[]>(fileNamesList[i].Item1, fileNamesList[i].Item2, fileNamesList[i].Item3));
-                            }
-                            else if (fileNamesList[i].Item1.Equals("SOILMOIST"))
-                            {
-                                for (int f = 0; f < fileNamesList[i].Item3.Length; f++)
-                                {
-                                    fileNamesList[i].Item3[f] = Regex.Replace(fileNamesList[i].Item3[j], "<([^;]*)>", "");
-                                }
-                                fileNamesList[i].Item3[j] = Regex.Replace(fileNamesList[i].Item3[j], "<([^;]*)>", "");
-                                clearList.Add(new Tuple<string, string, string[]>(fileNamesList[i].Item1, fileNamesList[i].Item2, fileNamesList[i].Item3));
-                            }
-                            else
-                            {
-                                Console.WriteLine("OH SHIT, SOMETHING WENT HORIBBLY WRONG");
-                            }
+                    for (int f = 0; f < fullDataList[i].Item3.Count(); f++)
+                    {
+                        if (String.IsNullOrEmpty(fullDataList[i].Item3[f]))
+                        {
+                        }
+                        else
+                        {                            
+                            fullDataList[i].Item3[f] = Regex.Replace(fullDataList[i].Item3[f], "<([^;]*)>", "");
+                         //   Console.WriteLine(fullDataList[i].Item3[f]);
+                            sensorData.Add(float.Parse(fullDataList[i].Item3[f], System.Globalization.NumberStyles.Any ,System.Globalization.CultureInfo.InvariantCulture));                           
                         }
                     }
+                    for (int j = 0; j < fullDataList.Count; j++)
+                    {
+                        if (dataType.Equals("TEMP"))
+                        {
+                            
+                            pureDataList.Add(new Tuple<string, string, List<float>>(dataType, collectionDate, sensorData.ToList()));                           
+                        }
+                        else if (dataType.Equals("HUM"))
+                        {
+                            pureDataList.Add(new Tuple<string, string, List<float>>(dataType, collectionDate, sensorData.ToList()));
+                        }
+                        else if (dataType.Equals("SOILMOIST"))
+                        {
+                            pureDataList.Add(new Tuple<string, string, List<float>>(dataType, collectionDate, sensorData.ToList()));
+                        }
+                        else
+                        {
+                            pureDataList.Add(new Tuple<string, string, List<float>>("UNKNOWN", "UNKNOWN", sensorData.ToList()));
+                        }
+
+                        dataType = String.Empty;
+                        collectionDate = String.Empty;
+                        sensorData.Clear();
+                    }
+
                 }
-                for (int i = 0; i < clearList.Count; i++)
+                for (int i = 0; i < pureDataList.Count; i++)
                 {
                     float averageData = 0;
-                    if (clearList[i].Item1.Equals("TEMP"))
+
+                    if (pureDataList[i].Item1.Equals("TEMP"))
                     {
-                        foreach (string line in clearList[i].Item3)
-                        {
-                            // Console.WriteLine(line);
-                            averageData += float.Parse(line, CultureInfo.InvariantCulture.NumberFormat);
-                        }
-                        averageData /= clearList[i].Item3.Count();
-                        File.WriteAllText(@"C:\Users\ivanf\Desktop\Sensors Data Tests\" + clearList[i].Item1 + clearList[i].Item2 + "-Pocessed on-" + DateTime.Now.ToLongDateString() + ".txt", "Average air temperature is: " + averageData.ToString());
-                    }
-                    else if (clearList[i].Item1.Equals("HUM"))
-                    {
-                        foreach (string line in clearList[i].Item3)
-                        {
-                            // Console.WriteLine(line);
-                            averageData += float.Parse(line, CultureInfo.InvariantCulture.NumberFormat);
-                        }
-                        averageData /= clearList[i].Item3.Count();
-                        File.WriteAllText(@"C:\Users\ivanf\Desktop\Sensors Data Tests\" + clearList[i].Item1 + clearList[i].Item2 + "-Pocessed on-" + DateTime.Now.ToLongDateString() + ".txt", "Average air humidity is: " + averageData.ToString());
-                    }
-                    else if ((clearList[i].Item1.Equals("SOILMOIST")))
-                    {
-                        foreach (string line in clearList[i].Item3)
-                        {
-                            // Console.WriteLine(line);
-                            averageData += float.Parse(line, CultureInfo.InvariantCulture.NumberFormat);
-                        }
-                        averageData /= clearList[i].Item3.Count();
+                        foreach (float value in pureDataList[i].Item3)
+                        {                           
+                            averageData += value;
+                        }                   
+                        averageData = averageData / pureDataList[i].Item3.Count();
                         
-                        File.WriteAllText(@"C:\Users\ivanf\Desktop\Sensors Data Tests\" + clearList[i].Item1 + clearList[i].Item2 + "-Pocessed on-" + DateTime.Now.ToLongDateString() + ".txt", "Average soil moisture is: " + averageData.ToString());
+                        File.WriteAllText(@"D:\Projects\Challenges\MarsTrack\MarsTrack\Data Gathering\Processed Data\" + pureDataList[i].Item1 + pureDataList[i].Item2 + "-Pocessed on-" + DateTime.Now.ToLongDateString() + ".txt", "Average air temperature is: " + averageData.ToString() + "°C");
+                    }
+                    else if (pureDataList[i].Item1.Equals("HUM"))
+                    {
+                        for (int m = 0; m < pureDataList[i].Item3.Count; m++)
+                        {                            
+                            averageData += pureDataList[i].Item3[m];
+                        }
+                                                    
+                        averageData = averageData / pureDataList[i].Item3.Count();
+                        File.WriteAllText(@"D:\Projects\Challenges\MarsTrack\MarsTrack\Data Gathering\Processed Data\" + pureDataList[i].Item1 + pureDataList[i].Item2 + "-Pocessed on-" + DateTime.Now.ToLongDateString() + ".txt", "Average air humidity is: " + averageData.ToString() + "%");
+                    }
+                    else if ((pureDataList[i].Item1.Equals("SOILMOIST")))
+                    {
+                        foreach (var value in pureDataList[i].Item3)
+                        {
+                            averageData += value;
+                        }
+                        averageData = averageData / pureDataList[i].Item3.Count();
+                        
+                        File.WriteAllText(@"D:\Projects\Challenges\MarsTrack\MarsTrack\Data Gathering\Processed Data\" + pureDataList[i].Item1 + pureDataList[i].Item2 + "-Pocessed on-" + DateTime.Now.ToLongDateString() + ".txt", "Average soil moisture is: " + averageData.ToString() + "%");
                     }
                     else
                     {
-
                     }
                 }
             }
